fb7fb64c788c97a8d4e209d70396b574e42460be
Aishwarya Sriram <98.aishu@gmail.com>
2025-04-27 19:12:09 -0700

    made the appbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,34 @@ export default function App() {
           <Button color="inherit" component={Link} to="/">Home</Button>
           <Button color="inherit" component={Link} to="/help">Help</Button>
         </Toolbar> */}
-        <Stack direction={"row"} justifyContent={"space-between"}>
-          <Stack direction={"row"} spacing={2.5} alignItems={"center"}>
-            <Box sx={{ width: 45, height: 45, bgcolor: '#004D9F' }} />
-            <Typography sx={{ fontSize: '24px', lineHeight: 1 }} fontWeight={700}>
-              Cloak
-            </Typography>
-          </Stack>
-          <Stack direction={"row"} alignItems={"center"} spacing={7.5}>
-            <Typography component="a" href="https://example.com/paper.pdf" target="_blank" rel="noopener noreferrer" sx={{ textDecoration: 'none', cursor: 'pointer' }}>Project Paper</Typography>
-            <Typography component="a" href="https://example.com/paper.pdf" target="_blank" rel="noopener noreferrer" sx={{ textDecoration: 'none', cursor: 'pointer' }}>Presentation</Typography>
-            <CTAButton label="Download Cloak" sx={{ height: "45px", fontSize: "16px"}}/>
-          </Stack>
-        </Stack>
+  <Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 3, md: 0 }} justifyContent="space-between" alignItems={{ xs: 'flex-start', md: 'center' }}>
+    <Stack direction="row" spacing={2.5} alignItems="center">
+      <Box sx={{ width: 45, height: 45, bgcolor: '#004D9F'}} />
+      <Typography sx={{ fontSize: 24, lineHeight: 1 }} fontWeight={700}>
+        Cloak
+      </Typography>
+    </Stack>
+
+  <Stack direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 2, sm: 5, md: 7.5 }} alignItems={{ xs: 'flex-start', sm: 'center' }} flexWrap="wrap">
+    <Typography component="a" href="https://example.com/paper.pdf" target="_blank" rel="noopener noreferrer"  sx={{ textDecoration: 'none', cursor: 'pointer' }}>
+      Project Paper
+    </Typography>
+
+    <Typography component="a" href="https://example.com/presentation.pdf" target="_blank" rel="noopener noreferrer" sx={{ textDecoration: 'none', cursor: 'pointer' }}>
+      Presentation
+    </Typography>
+
+    <CTAButton
+      label="Download Cloak"
+      sx={{
+        height: 45,
+        fontSize: 16,
+        flexShrink: 0,               // keep button from squeezing
+        width: { xs: '100%', sm: 'auto' },  // full-width on phones, auto later
+      }}
+    />
+  </Stack>
+</Stack>
       </AppBar>
       <Routes>
         <Route path="/" element={<Home />} />
