1efce6989a53870fb4bf1def0aaab45d0b2f2a9b
Aishwarya Sriram <98.aishu@gmail.com>
2025-05-07 16:30:36 -0700

    added new cloak logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Box, Typography, Stack } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import { AppBar, Typography, Stack } from '@mui/material';
 
 import Home from './components/Home';
 import Help from './components/Help';
 import CTAButton from './components/CTAButton';
+import logo from './assets/logo.svg';
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
       }} elevation={0}>
   <Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 3, md: 0 }} justifyContent="space-between" alignItems={{ xs: 'flex-start', md: 'center' }}>
     <Stack direction="row" spacing={2.5} alignItems="center">
-      <Box sx={{ width: 45, height: 45, bgcolor: '#004D9F'}} />
+      <img src={logo} alt="Cloak logo" style={{ width: 45, height: 45 }} />
       <Typography sx={{ fontSize: 24, lineHeight: 1 }} fontWeight={700}>
         Cloak
       </Typography>
